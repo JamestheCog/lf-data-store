48525ccdacde14c2e996cda8e85ef6ca5a96d5f5
JamestheCog <jcog196013@gmail.com>
2024-10-04 09:46:50 +0800

    Removed route imports for dashboard

--- app.py
+++ app.py
@@ -1,23 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
 
 # Import the routes here:
 from routes.db import db
-from routes.dashboard import dashboard
 
 ## Application start and route registration:
 app = Flask(__name__)
 app.register_blueprint(db)
-app.register_blueprint(dashboard)
-
-## Error handling in the application (meant for when somebody accesses the application):
-@app.errorhandler(403)
-def forbidden_access(e):
-    return(render_template('./errors/page-403.html', e = e), 403)
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return(render_template('./errors/page-404.html', e = e), 404)
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return(render_template('./errors/page-500.html', e = e), 500)
