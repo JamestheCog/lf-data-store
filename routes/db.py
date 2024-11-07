c2893f043916ab040d58adbf82705cad5adadd1a
JamestheCog <jcog196013@gmail.com>
2024-11-07 16:35:08 +0800

    Use json library to handle JSON strings for some routes

--- routes/db.py
+++ routes/db.py
@@ -18,7 +18,7 @@ def fetch_information():
     Tries to fetch information from the database provided that the access password has been given.  If not,
     return an error and do not return any data.
     '''
-    data = request.get_json()
+    data = json.loads(request.get_json())
     results, code = db_funcs.fetch_data(data.get('authorization', dict()).get('password'))
     return(jsonify(results), code)
 
@@ -50,7 +50,7 @@ def update_information():
     to be supplied:
     '''
     try:
-        data = request.get_json() ; password = data.get('authorization', dict()).get('password', None)
+        data = json.loads(request.get_json()) ; password = data.get('authorization', dict()).get('password', None)
         if password is None or password != os.getenv('PASSWORD'):
             return(jsonify({'status_code' : 403, 'message' : 'incorrect / missing password'}), 403)
         elif len(data.get('data')) <= 2:
@@ -85,7 +85,7 @@ def delete_patient():
     Given a patient's NRIC and Name, delete them from the database:
     '''
     try:
-        data = request.get_json() ; password = data.get('authorization', dict()).get('password', None)
+        data = json.loads(request.get_json()) ; password = data.get('authorization', dict()).get('password', None)
         if password is None or password != os.getenv('PASSWORD'):
             return(jsonify({'status_code' : 403, 'message' : 'incorrect / missing password'}), 403)
         elif len(data.get('data')) > 3:
